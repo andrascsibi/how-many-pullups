4ce0b5751dded18aff7896268c4f61fab497001d
andras <andras.csibi@gmail.com>
2014-05-17 18:29:40 +0200

    routing with mux

--- src/accounts/accounts.go
+++ src/accounts/accounts.go
@@ -1,180 +1,253 @@
 package accounts
 
 import (
-    "net/http"
+	"net/http"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 
-    "time"
+	"time"
 
-    "crypto/sha1"
-    "io"
-    "encoding/base64"
-//    "errors"
-    // "strconv"
-//     "fmt"
-//    "regexp"
+	"crypto/sha1"
+	"encoding/base64"
+	"io"
+	//    "errors"
+	// "strconv"
+	"fmt"
+	//    "regexp"
 
-    "errors"
-    "strings"
-    "appengine"
-    "appengine/user"
-    "encoding/json"
-    "appengine/datastore"
-
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
+	"encoding/json"
+	"errors"
 )
 
 type Account struct {
-    Email      string
-    ID         string
-    ScreenName string
-    Admin      bool
-    RegDate    time.Time
+	Email      string
+	ID         string
+	ScreenName string
+	Admin      bool
+	RegDate    time.Time
 
-    Challenges []string
-    Settings   Settings
+	Challenges []string
+	Settings   Settings
 }
 
 type Settings struct {
-
 }
 
 type Challenge struct {
-  Title  string
-  Description string
-  Active bool
+	Title       string
+	Description string
+	Active      bool
 }
 
 func init() {
-    r := mux.NewRouter()
-    r.HandleFunc("/whoami", whoami)
-    r.HandleFunc("/accounts/", accounts)
-    http.Handle("/", r)
+	r := mux.NewRouter()
+	r.HandleFunc("/whoami", whoami)
+
+	r.HandleFunc("/accounts/",
+		createAccount).
+		Methods("POST")
+
+	r.HandleFunc("/accounts/{accountId}",
+		getAccount).
+		Methods("GET")
+	r.HandleFunc("/accounts/{accountId}",
+		updateAccount).
+		Methods("POST")
+
+	r.HandleFunc("/accounts/{accountId}/challenges",
+		getChallenges).
+		Methods("GET")
+	r.HandleFunc("/accounts/{accountId}/challenges",
+		createChallenge).
+		Methods("POST")
+
+	r.HandleFunc("/accounts/{accountId}/challenges/{challengeId}",
+		getChallenge).
+		Methods("GET")
+	r.HandleFunc("/accounts/{accountId}/challenges/{challengeId}",
+		updateChallenge).
+		Methods("POST")
+
+	r.HandleFunc("/accounts/{accountId}/challenges/{challengeId}/sets",
+		getSets).
+		Methods("GET")
+	r.HandleFunc("/accounts/{accountId}/challenges/{challengeId}/sets",
+		createSet).
+		Methods("POST")
+	r.HandleFunc("/accounts/{accountId}/challenges/{challengeId}/sets",
+		importSets).
+		Methods("PUT")
+
+	http.Handle("/", r)
 }
 
-func accounts(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    path := strings.Split(r.URL.Path, "/")
-    if len(path) < 3 {
-        return
-    }
-    accountId := path[2]
+func createAccount(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	c.Infof("creating account")
+}
 
-    var account Account
-    err := datastore.Get(c, accountKey(c, accountId), &account)
+func getAccount(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	accountId := mux.Vars(r)["accountId"]
+	c.Infof("get account %v", accountId)
 
-    if err == datastore.ErrNoSuchEntity {
-        http.Error(w, err.Error(), http.StatusNotFound)
-        return;
-    }
+	var account Account
+	err := datastore.Get(c, accountKey(c, accountId), &account)
 
-    // /account/{a_id} (element)
-    if len(path) == 3 {
-        // GET: public account info
-        // POST: update info, like screen name (login required)
-        return
-    }
+	if err == datastore.ErrNoSuchEntity {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
 
-    // /account/{a_id}/challenges (collection)
-    if len(path) == 4 && path[3] == "challenges" {
-        c.Infof("challenges collection for %v", accountId)
-        // GET: list challenges
-        // POST: create new challenge
-        return
-    }
+	w.Header().Set("Content-type", "application/json")
 
-    // /accounts/{id}/challenges/{c_id} (element)
-    if len(path) == 5 && path[3] == "challenges" {
-        // GET: title, description
-        // POST: update title/description
-        return
-    }
+	ret, err := json.Marshal(Account{
+		Email: account.Email,
+	})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Write(ret)
+}
 
-    // /accounts/{id}/challenges/{c_id}/sets (collection)
-    if len(path) == 6 && path[3] == "challenges" && path[5] == "sets" {
-        // GET: list of all sets (export)
-        // POST: create new set (param: reps)
-        // PUT: replace whole collection (import)
-        return
-    }
+func updateAccount(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	accountId := mux.Vars(r)["accountId"]
+	_ = c
+	fmt.Fprintf(w, "updating account %v\n", accountId)
+}
 
+func getChallenges(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	accountId := mux.Vars(r)["accountId"]
+	_ = c
+	fmt.Fprintf(w, "listing challenges for account %v\n", accountId)
+}
+
+func createChallenge(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	accountId := mux.Vars(r)["accountId"]
+	_ = c
+	fmt.Fprintf(w, "creating challenge for account %v\n", accountId)
+}
+
+func getChallenge(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	accountId := mux.Vars(r)["accountId"]
+	challengeId := mux.Vars(r)["challengeId"]
+	_ = c
+	fmt.Fprintf(w, "listing challenge %v/%v\n", accountId, challengeId)
+}
+
+func updateChallenge(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	accountId := mux.Vars(r)["accountId"]
+	challengeId := mux.Vars(r)["challengeId"]
+	_ = c
+	fmt.Fprintf(w, "updating challenge title/descr %v/%v\n", accountId, challengeId)
+}
+
+func getSets(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	accountId := mux.Vars(r)["accountId"]
+	challengeId := mux.Vars(r)["challengeId"]
+	_ = c
+	fmt.Fprintf(w, "getting sets of %v/%v\n", accountId, challengeId)
+}
+
+func createSet(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	accountId := mux.Vars(r)["accountId"]
+	challengeId := mux.Vars(r)["challengeId"]
+	_ = c
+	fmt.Fprintf(w, "creating set for %v/%v\n", accountId, challengeId)
+}
+
+func importSets(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	accountId := mux.Vars(r)["accountId"]
+	challengeId := mux.Vars(r)["challengeId"]
+	_ = c
+	fmt.Fprintf(w, "importing sets to %v/%v\n", accountId, challengeId)
 }
 
 func getOrCreateAccount(c appengine.Context) (account Account, err error) {
-    u := user.Current(c)
-    if u == nil {
-        err = errors.New("Login required.")
-        return
-    }
-    email := u.Email
-    id := hash(u.ID)
+	u := user.Current(c)
+	if u == nil {
+		err = errors.New("Login required.")
+		return
+	}
+	email := u.Email
+	id := hash(u.ID)
 
-    key := accountKey(c, id)
+	key := accountKey(c, id)
 
-    err = datastore.Get(c, key, &account)
+	err = datastore.Get(c, key, &account)
 
-    if err == datastore.ErrNoSuchEntity {
-        c.Infof("creating new user: %v %v", email, id)
-        account = Account {
-            Email: email,
-            ID: id,
-            RegDate: time.Now(),
-        }
-        _, err = datastore.Put(c, key, &account)
-    }
-    return
+	if err == datastore.ErrNoSuchEntity {
+		c.Infof("creating new user: %v %v", email, id)
+		account = Account{
+			Email:   email,
+			ID:      id,
+			RegDate: time.Now(),
+		}
+		_, err = datastore.Put(c, key, &account)
+	}
+	return
 }
 
 func accountKey(c appengine.Context, id string) *datastore.Key {
-    return datastore.NewKey(c, "Accounts", id, 0, nil)
+	return datastore.NewKey(c, "Accounts", id, 0, nil)
 }
 
 func whoami(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    u := user.Current(c)
+	c := appengine.NewContext(r)
+	u := user.Current(c)
 
-    var ret LoginData
-    w.Header().Set("Content-type", "application/json")
+	var ret LoginData
+	w.Header().Set("Content-type", "application/json")
 
-    if u == nil {
-        url, err := user.LoginURL(c, "")
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-        ret.LoginURL = url
-    } else {
-        url, err := user.LogoutURL(c, "")
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-        ret.LogoutURL = url
+	if u == nil {
+		url, err := user.LoginURL(c, "")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		ret.LoginURL = url
+	} else {
+		url, err := user.LogoutURL(c, "")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		ret.LogoutURL = url
 
-        account, err := getOrCreateAccount(c)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
-        ret.Account = account
-    }
+		account, err := getOrCreateAccount(c)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		ret.Account = account
+	}
 
-    loginData, err := json.Marshal(ret)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
-    w.Write(loginData)
+	loginData, err := json.Marshal(ret)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Write(loginData)
 }
 
 type LoginData struct {
-    Account Account
-    LoginURL string
-    LogoutURL string
+	Account   Account
+	LoginURL  string
+	LogoutURL string
 }
 
 func hash(id string) string {
-    hasher := sha1.New()
-    io.WriteString(hasher, id)
-    io.WriteString(hasher, "salt it real good DbqOFzkk") // TODO: should come from environment
-    return base64.URLEncoding.EncodeToString(hasher.Sum(nil))[:8]
+	hasher := sha1.New()
+	io.WriteString(hasher, id)
+	io.WriteString(hasher, "salt it real good DbqOFzkk") // TODO: should come from environment
+	return base64.URLEncoding.EncodeToString(hasher.Sum(nil))[:8]
 }
-
