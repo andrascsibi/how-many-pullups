7576cc2bf09233b86663910b58cbe4961ccb3beb
andras <andras.csibi@gmail.com>
2014-05-18 00:34:08 +0200

    pretty accounts page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -48,6 +48,31 @@ angular.module('pullApp')
 .controller('AdminCtrl', ['$scope', '$resource', function($scope, $resource) {
   var Account = $resource("/accounts/:id", {id: '@id'}, {});
   $scope.accounts = Account.query();
+
+  $scope.selected = null;
+
+  $scope.list = function(idx){
+    Account.query(function(data){
+      $scope.accounts = data;
+      if(idx !== undefined) {
+        $scope.selected = $scope.accounts[idx];
+        $scope.selected.idx = idx;
+      }
+    }, function(error){
+      alert(error.data);
+    });
+  };
+
+  $scope.list();
+
+  $scope.get = function(idx){
+    Account.get({id: $scope.accounts[idx].ID}, function(data){
+      $scope.selected = data;
+      $scope.selected.idx = idx;
+    });
+  };
+
+
   // $scope.list = function(idx){
   //   // Notice calls to Book are often given callbacks.
   //   Book.query(function(data){
