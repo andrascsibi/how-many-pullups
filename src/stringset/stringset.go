231b693953548780afdc46e0197b2257b1756120
andras <andras.csibi@gmail.com>
2014-07-26 14:32:59 +0200

    gochopping: extracted stringset

---
+++ src/stringset/stringset.go
@@ -0,0 +1,24 @@
+package stringset
+
+func IndexOf(list []string, item string) int {
+  for i, s := range list {
+    if s == item {
+      return i
+    }
+  }
+  return -1
+}
+func Add(list []string, item string) []string {
+  i := IndexOf(list, item)
+  if i >= 0 {
+    return list
+  }
+  return append(list, item)
+}
+func Remove(list []string, item string) []string {
+  i := IndexOf(list, item)
+  if i < 0 {
+    return list
+  }
+  return append(list[:i], list[i+1:]...)
+}
